139d60f4ebfe6af0604f227d6964ea09ed1786db
pukiediederik <diederik.boeren@gmail.com>
2021-06-07 16:13:53 +0200

    ADD: solver

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,27 @@
+#%% imports
+from copy import deepcopy
+
 #%% the sudoku itself
 class Sudoku:
     #input can be a 81 character long string
-    def __init__(self, input : str):
-        def parseString(input: str):
+    def __init__(self, input):
+        if(type(input) == str):
             if (len(input) == 81):
-                return [int(x) for x in input]
+                self.value = [int(x) for x in input]
             else:
-                print("length of the string was not 81 characters")
-                return None
+                print("length of the input was not 81 characters")
+                self.value = None
 
-        self.value = parseString(input)
+        elif(type(input) == list):
+            if(len(input) == 81):
+                self.value = input
+            else:
+                print("length of the input was not 81 characters")
+                self.value = None
 
-    #this function will also return false if the number is the same at the position
+    def __deepcopy__(self, memo):
+        return type(self)(deepcopy(self.value))
+
     def checkIfValidAt(self, pos, value):
         if(pos > 80): print("position out of range")
         elif(value > 9): print("value out of range")
@@ -21,18 +31,18 @@ class Sudoku:
             horizontalBase = pos - horizontalOffset
 
             for x in range(horizontalBase, horizontalBase + 9):
-                if(self.value[x] == value): return False
+                if(self.value[x] == value and x != pos): return False
 
             #if the number is in the same column
             for y in range(horizontalOffset, 81, 9):
-                if(self.value[y] == value): return False
+                if(self.value[y] == value and y != pos): return False
 
             #if the number is in the same section (3x3)
             # vertical section + horizontal section
             sectionStart = (pos - (pos % 27)) + ((pos % 9) - (pos % 3))
             for y in range(sectionStart, 27, 9):
                 for x in range(3):
-                    if(self.value[x + y] == value): return False
+                    if(self.value[x + y] == value and x + y != pos): return False
 
             #if there is no reason why you cant place that number there, return true
             return True
@@ -49,5 +59,27 @@ class Sudoku:
 
 #%% this will solve the sudoku (takes in a sudoku as input)
 def solveSudoku(sudoku: Sudoku):
-    
-    return None
+    unsolvedSudoku = deepcopy(sudoku)
+    currentPos = 0
+    direction = 1 #1 forward, -1 backward
+    #loop until finished
+    while(0 >= currentPos < 81):
+        if(sudoku.value[currentPos] != 0): currentPos += direction
+        else:
+            direction = 1
+            #search for the next valid position
+            unsolvedSudoku.value[currentPos] += 1
+            while not (unsolvedSudoku.checkIfValidAt(currentPos, unsolvedSudoku.value[currentPos])):
+                unsolvedSudoku.value[currentPos] += 1
+                print("value: " + str(unsolvedSudoku.value[currentPos]))
+                if (unsolvedSudoku.value[currentPos] > 9):
+                    unsolvedSudoku.value[currentPos] = 0
+                    direction = -1
+                    currentPos -= 1
+                    break
+
+            if (direction == 1):
+                currentPos += 1
+
+    return unsolvedSudoku
+# %%
